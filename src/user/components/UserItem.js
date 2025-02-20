708d829c23de7b06fb6838145a6b4c8bcbcdd01f
SirajDilshan <sirajdilshan789@gmail.com>
2025-02-20 22:41:26 +0530

    styling our app

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './UserItem.css';
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
+import { Link } from 'react-router-dom';
 
  export const UserItem = props => {
   return (
     <li className='user-item'>
         <div className='user-item__content'>
+            <Card>
+            <Link to={`/${props.id}/places`}>
             <div className='user-item__image'>
-<img src={props.image} alt={props.name} />
+<Avatar image={props.image} alt={props.name} />
             </div>
             <div className='user-item__info'>
                 <h2>{props.name}</h2>
                 <h3>{props.placeCount} {props.placeCount === 1 ? 'place' : 'places'}</h3>
 
             </div>
+            </Link>
+            </Card>
         </div>
     </li>
   )
