bf03b159848d0ac1fe78ddd9399875ab9e41ac1f
SirajDilshan <sirajdilshan789@gmail.com>
2025-02-21 22:44:08 +0530

    Adding a mainHeader

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NavLinks = () => {
+  return (
+    <div>NavLinks</div>
+  )
+}
+export default NavLinks;
--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { UserItem } from "./UserItem";
 import './UserList.css';
+import Card from '../../shared/components/UIElements/Card';
 
 export const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h1>No User Found ....</h1>
+        <Card>
+<h1>No User Found ....</h1>
+        </Card>
+        
       </div>
     );
   }
